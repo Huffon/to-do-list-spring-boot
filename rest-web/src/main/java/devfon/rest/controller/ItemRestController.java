dfb81b9eac7946e5144a636747dfb249d930996b
huffonism@gmail.com <huffonism@gmail.com>
2018-11-03 16:52:28 +0900

    connect mysql server

--- rest-web/src/main/java/devfon/rest/controller/ItemRestController.java
+++ rest-web/src/main/java/devfon/rest/controller/ItemRestController.java
@@ -4,6 +4,7 @@ import devfon.rest.entity.Item;
 import devfon.rest.repository.ItemRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.PagedResources.PageMetadata;
@@ -27,7 +28,7 @@ public class ItemRestController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getItems(
-            @PageableDefault Pageable pageable
+            @PageableDefault(sort = {"deadline"}, direction = Sort.Direction.DESC) Pageable pageable
     ) {
         Page<Item> items = itemRepository.findAll(pageable);
         PageMetadata pageMetadata = new PageMetadata(pageable.getPageSize(),
--- rest-web/src/main/java/devfon/rest/repository/ItemRepository.java
+++ rest-web/src/main/java/devfon/rest/repository/ItemRepository.java
@@ -4,5 +4,4 @@ import devfon.rest.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Item findByTitle(String title);
 }
